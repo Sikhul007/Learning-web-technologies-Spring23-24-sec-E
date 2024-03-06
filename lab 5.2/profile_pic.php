4c735096346053f73b206659fa4592b5bfd9dad9
Sikhul007 <sikhulshihab@gmail.com>
2024-03-06 21:51:39 +0600

    Design

--- lab 5.2/profile_pic.php
+++ lab 5.2/profile_pic.php
@@ -1,21 +1,58 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Form</title>
+    <title>View profile</title>
 </head>
 <body>
 
-    <form method="post" action="1.php"enctype = "">
-        <fieldset>
-            <legend><b>EDIT PROFILE</b></legend>
-                 NAME:                      <input type="text" name="name" value="" /> <br><hr>
-                 Email:                     <input type="text" name="Email" value="" /><br><hr>
-                 Gender:                    <input type="radio" name="gender" value="" /> Male
-                                            <input type="radio" name="gender" value="" /> Female
-                                            <input type="radio" name="gender" value="" /> Other <br><hr>
-                 Date of Birth:             <input type="text" name="DateofBirth" value="" /><br><br><hr>  
-                                            <br><input type="submit" name="" value="Submit" />
-        </fieldset>
-    </form>
+    <?php
+        // PHP code for dynamic content
+        $currentYear = date("Y");
+    ?>
+    <table border="1" cellspacing="0" width="700px" align="center">
+        <tr height="100px" style="padding: 10px;">
+            <td style="border-right: none;">
+                <img src="1.png" alt="">
+            </td>
+            <td align="right" style="border-left: none;" colspan="2">
+                Logged in as <a href="view_profile.php">Bob</a> |
+                <a href="logout.php">Logout</a> 
+            </td>
+        </tr>
+
+       <tr>
+        <th align="left" style="padding: 10px;">
+            Account<br><hr>
+                <ul>
+                    <li><a href="dash_board.php">Dashboard</a></li>
+                    <li><a href="view_profile.php">View profile</a></li>
+                    <li><a href="edit_profile.php">Edit Profile</a><br></li>
+                    <li><a href="change_pp.php">Change Profile Picture</a><br></li>
+                    <li><a href="change_pass.php">Change Password</a><br></li>
+                    <li><a href="logout.php">Logout</a><br></li> 
+                </ul>
+            </th>
+            
+            <td style="border-right: none;">
+                <form method="post" action="" enctype = "">
+                    <fieldset >
+                        <legend><b>PROFILE PICTURE</b></legend>
+                        <img src="profile.png" alt="" height="100px" width="100px" style="padding: 10px;"><br>
+                        Picture <input type="file" name="" value=""><br>
+                        <hr>
+                        <input type="submit" name="" value="Submit">
+                    </form>
+                </td>
+            </fieldset>  
+            </tr>
+                
+        <tr>
+            <td colspan="3" align="center" height="25px">
+                Copyright &copy; <?php echo $currentYear; ?>
+            </td>
+        </tr>
+
+    </table>
+
 </body>
 </html>
